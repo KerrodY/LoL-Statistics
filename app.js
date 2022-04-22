334ba6b00132562dc34536858191d33d569833c0
KerrodY <kerrodyoung@gmail.com>
2022-04-22 14:17:18 +1000

    Setup Express

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//Binds to default computer port ie. `localhost` or `127.0.0.1` etc
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
