ffc316f5e1584f28e047d4ed894894a3aa46aa17
KerrodY <kerrodyoung@gmail.com>
2022-04-22 22:07:47 +1000

    Call Riot API and get summoner data

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 require('dotenv').config();
 
+const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const apiKey = process.env.RIOT_API_KEY
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('summoner/:accountName', (req, res) => {
+  
+  // Make a request for a user with a given ID
+  axios.get(`https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.accountName}?api_key=${apiKey}`)
+    .then(function (response) {
+      // handle success
+      res.send(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 })
-
 //Binds to default computer port ie. `localhost` or `127.0.0.1` etc
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
