8b1e3c1756c8f0f98a1cd38f35747dec9fb53bf8
Isaiah Weidl <2137144+IWeidl@users.noreply.github.com>
2022-04-22 22:12:45 +1000

    Installed vue.js

---
+++ lolStatClient/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>
+
+      <script>
+        import axios from "axios";
+        axios.get("localhost:3000/summoner/Exlex")
+            .then(function (response) {
+          // handle success
+          console.log(response.data)
+            })
+            .catch(function (error) {
+              // handle error
+              console.log(error);
+            })
+            .then(function () {
+              // always executed
+            });
+
+      </script>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
