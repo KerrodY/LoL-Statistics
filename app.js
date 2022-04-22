74c3672e3303cc0ed808cdbef1799c6849845107
KerrodY <kerrodyoung@gmail.com>
2022-04-22 14:29:04 +1000

    Setup environment variable file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
