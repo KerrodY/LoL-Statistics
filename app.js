1fde8281e6cf6a632c8105eb0cde9d2d3fabaa98
KerrodY <kerrodyoung@gmail.com>
2022-04-27 15:24:21 +1000

    Get summoners last 20 game IDs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const axios = require('axios')
 const express = require('express')
 const app = express()
+
+//TODO: this allows anyone to make request, not secure but probably okay for an API. Needs more thought.
 app.use(cors())
 
 const port = process.env.PORT
@@ -14,16 +16,17 @@ app.get('/:region/summoner/:accountName', (req, res) => {
   axios.get(`https://${req.params.region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.accountName}?api_key=${apiKey}`)
     .then(function (response) {
       // handle success
-      console.log(response.data)
-      res.send(response.data)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+      axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${response.data.puuid}/ids?start=0&count=20&api_key=${apiKey}`)
+        .then(function (response2) {
+          data = response.data
+          data.gameIds = response2.data
+          res.send(data)
+        })
+        .catch(function (error) {console.log(error); })
+        .then(function () { });
+      })
+    .catch(function (error) { console.log(error); })
+    .then(function () { });
 })
 //Binds to default computer port ie. `localhost` or `127.0.0.1` etc
 app.listen(port, () => {
